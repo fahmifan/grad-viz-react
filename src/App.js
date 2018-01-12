d68ff3e1e1dd673601676fa2e8e15ce0f08eab11
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-12 09:49:57 +0700

    New cra & tachyons

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <header className='tc pv4 pv5-ns'>
+        <img src={logo} className='br-100 pa1 ba b--black-10 h3 w3' alt='avatar' />
+        <h1 className='f5 f4-ns fw6 mid-gray'>Jasper Whitehouse</h1>
+        <h2 className='f6 gray fw2 ttu tracked'>Los Angeles</h2>
+      </header>
+    );
+  }
+}
+
+export default App;
