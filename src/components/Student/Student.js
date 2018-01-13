58c558def39391b0d01632baceded782fa30d4e3
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-13 13:42:06 +0700

    Add Student component like card

---
+++ src/components/Student/Student.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import logo from '../../logo.svg';
+
+const student = (props) => (
+  <section className="tc pa2 dib">
+    <article className="hide-child relative ba b--black-10 mw4 center pointer grow helvetica br4 shadow-4">
+    <div className="h-100">  
+      <img 
+        src={logo} 
+        alt="Photo of John Doe" 
+        className="db h4" />
+    </div>
+      <div className="pa2 bt b--black-20">
+        <p className="f4 db link mv1 dark-blue hover-blue pointer">John Doe</p> 
+        <p className="f5 gray mv1">140810160028</p>
+      </div>
+    </article>
+  </section>
+);
+
+export default student;
