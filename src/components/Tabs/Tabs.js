5e19724f1c03840589118b3f18888cc6a873caba
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-14 21:49:46 +0700

    Made Tabs of Subject component

---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const tabs = () => (
+  <ul className="center mb0 pa2 link black no-underline">
+    <a className="pointer pa2 ma0 bt br bl b--black-20 br3 br--top">Math</a>
+    <a className="pointer pa2 ma0">Indo</a>
+    <a className="pointer pa2 ma0">Natural Science</a>
+    <a className="pointer pa2 ma0">English</a>
+  </ul>
+);
+
+export default tabs;
