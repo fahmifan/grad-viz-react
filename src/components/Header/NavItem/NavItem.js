a14b4e93ce738e6567ac22070a8aec1d7cc9f6a3
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-14 17:12:33 +0700

    import Link from react-router-dom

--- src/components/Header/NavItem/NavItem.js
+++ src/components/Header/NavItem/NavItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const navItem = (props) => (
     <a href="/" className={`link dim gray f6 ${props.header ? props.header : 'f5-ns'} dib mr3 helvetica`}>
