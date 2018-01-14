6b8f8434d076706c0b68495557ea512fa1fd3e84
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-14 17:11:41 +0700

    Add Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const home = () => (
+  <div className="tc helvetica mt3">
+    <h1>Welcome TO Gradviz</h1>
+    <p>We are in developing mode</p>
+    <h3>We will available soon,see you :)</h3>
+  </div>
+);
+
+export default home;
