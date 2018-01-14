86a62567cd3aa14419a6440463dcfbb20a342fdb
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-14 16:27:44 +0700

    fix Student UI

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -4,12 +4,12 @@ import picture from '../../assets/profile_fahmi.jpg';
 
 const student = (props) => (
   <section className="tc pa2 dib">
-    <article className="hide-child relative ba b--black-10 mw4 center pointer grow helvetica br4 shadow-4">
+    <article className="hide-child relative ba b--black-10 mw4 center pointer grow dim helvetica br4 shadow-4">
     <div className="h-100">  
       <img 
         src={picture} 
         alt="Photo of John Doe" 
-        className="db h4" />
+        className="db center mw-100 br--top br4" />
     </div>
       <div className="pa2 bt b--black-20">
         <p className="f4 db link mv1 dark-blue hover-blue pointer">John Doe</p> 
