689f38eddbeb26eeb1c11e2ed038fd4001b26151
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-14 17:12:08 +0700

    Add link props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,10 @@ import NavItem from './NavItem/NavItem';
 const header = () => (
   <span>
     <nav class="pa pa4-ns">
-        <NavItem header="b f4-ns">Gradviz</NavItem>
-        <NavItem>Students</NavItem>
-        <NavItem>Tutorial</NavItem>
-        <NavItem>Log Out</NavItem>
+        <NavItem header="b f4-ns" link="/">Gradviz</NavItem>
+        <NavItem link="/students">Students</NavItem>
+        <NavItem link="/tutorial">Tutorial</NavItem>
+        <NavItem link="/logout">Log Out</NavItem>
     </nav>
   </span>
 );
