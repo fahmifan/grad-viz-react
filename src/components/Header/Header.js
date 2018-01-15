c86e23ecd52dea398bc07997adf6c6a85eeb54f5
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-15 13:35:53 +0700

    remove span and add fixed when scroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,12 @@ import React from 'react';
 import NavItem from './NavItem/NavItem';
 
 const header = () => (
-  <span>
-    <nav className="pa2 pa4-ns">
-        <NavItem header="b f4-ns" link="/">Gradviz</NavItem>
-        <NavItem link="/students">Students</NavItem>
-        <NavItem link="/tutorial">Tutorial</NavItem>
-        <NavItem link="/logout">Log Out</NavItem>
-    </nav>
-  </span>
+  <nav className="fixed dib w-100 h3 flex items-center center pa2 pa4-ns shadow-4 z-999 bg-white">
+      <NavItem header="b f4-ns" link="/">Gradviz</NavItem>
+      <NavItem link="/students">Students</NavItem>
+      <NavItem link="/tutorial">Tutorial</NavItem>
+      <NavItem link="/logout">Log Out</NavItem>
+  </nav>
 );
 
 export default header;
