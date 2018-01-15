dbc2209b5820f01502fc818866c377e1aae9646b
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-15 13:50:41 +0700

    change to `w-80` & `center`

--- src/containers/Students/Students.js
+++ src/containers/Students/Students.js
@@ -11,7 +11,7 @@ class Students extends Component {
 
   render() {
     return (
-      <div className="w-90 vh-100 pa2 flex flex-wrap mt5 ml-auto">
+      <div className="w-80 vh-100 pa2 flex flex-wrap mt5 center">
         <Student clicked={() => this.onClickHandler(1)} />
         <Student clicked={() => this.onClickHandler(1)} />
         <Student clicked={() => this.onClickHandler(1)} />
