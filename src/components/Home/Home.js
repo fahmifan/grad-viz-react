1bdff9efdb41abba877ae752764e57f2bf82593f
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-15 13:36:54 +0700

    change to adapt navbar scroll fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const home = () => (
-  <div className="tc helvetica mt3">
+  <div className="vh-100 mt5 pt2 border-box relative db tc helvetica">
     <h1>Welcome TO Gradviz</h1>
     <p>We are in developing mode</p>
     <h3>We will available soon,see you :)</h3>
