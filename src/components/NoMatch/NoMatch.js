709bb8bd0679a303cd70080dfe0c14de6a192d27
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 19:34:03 +0700

    fix NoMatch.js height

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const noMathc = () => <h1 className="mt6 pv3 tc relative i f1 ba br2 b--black-10">Sorry Page Not Found</h1>;
+const noMathc = () => <h1 className="vh-100 mt6 pv3 tc relative i f1 ">Sorry Page Not Found</h1>;
 
 export default noMathc;
