d7b5f86099b9e66596941529e55f2a549cec1ff9
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 17:57:28 +0700

    replace a-tag with TabItem

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const tabs = () => (
-  <ul className="center mb0 pa2 link black no-underline">
-    <a className="pointer pa2 ma0 bt br bl b--black-20 br3 br--top">Math</a>
-    <a className="pointer pa2 ma0">Indo</a>
-    <a className="pointer pa2 ma0">Natural Science</a>
-    <a className="pointer pa2 ma0">English</a>
+import TabItem from './TabItem/TabItem';
+
+const tabs = (props) => (
+  <ul className="center mb0 pl0 pv2 pb1 bb b--black-30">
+    <TabItem link={`${props.url}/math`} >Math</TabItem>
+    <TabItem link={`${props.url}/indonesia`} >Indonesia</TabItem>
+    <TabItem link={`${props.url}/science`} >Science</TabItem>
+    <TabItem link={`${props.url}/english`} >English</TabItem>
   </ul>
 );
 
