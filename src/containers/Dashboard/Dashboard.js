c771dea5238169e5eee2c2be9b69a2345204c6da
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 17:59:41 +0700

    fix UI

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Tabs from '../../components/Tabs/Tabs';
 import StudentForm from '../../components/StudentForm/StudentForm';
 import Student from '../../components/Student/Student';
+import Table from '../../components/Table/Table';
 
 class Dashoard extends Component {
 
@@ -12,10 +13,11 @@ class Dashoard extends Component {
 
   render() {
     return (
+      <div className="w-100 w-80-ns h-100 mt5 pt2 center relative">
         <Tabs url={this.props.match.url} />
         <div className="flex w-95 h-100 mt0 ph2 pv3 flex flex-wrap">
           <StudentForm />
-          <span className="center db">
+          <span className="center mr0-ns mv3 pt3 db">
             <Student />
             <button
               onClick={() => this.statsClickHandler(1)} 
@@ -24,6 +26,9 @@ class Dashoard extends Component {
             <button 
               className="db center pointer shadow-4 w4 f6 link dim br3 ph3 pv2 mb2 white bg-blue bw0 overflow-hidden"
               >Compare</button>
+          </span>
+          <span className="mt3 w-100">
+            <Table />
           </span>
         </div>
       </div>
