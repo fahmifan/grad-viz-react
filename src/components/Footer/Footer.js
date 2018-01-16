d48f629e51893d026643278cf5a92c614d2fa55f
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 18:54:46 +0700

    Made footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => (
+  <footer>
+    <div className="tc helvetica f5 pv3 white bg-gray">
+      Gradviz &copy; 2017
+    </div>
+  </footer>
+);
+
+export default footer;
