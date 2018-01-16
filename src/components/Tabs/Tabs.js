bd2375d7f8b0956d37c0ef7a474648a5ea5798b0
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 19:34:43 +0700

    Add overflow-scroll in small screen

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -3,7 +3,7 @@ import React from 'react';
 import TabItem from './TabItem/TabItem';
 
 const tabs = (props) => (
-  <ul className="center mb0 pl0 pv2 pb1 bb b--black-30">
+  <ul className="center overflow-x-scroll overflow-x-hidden-ns border-box mv2 pl0 pv2 pb2 bb b--black-30">
     <TabItem link={`${props.url}/math`} >Math</TabItem>
     <TabItem link={`${props.url}/indonesia`} >Indonesia</TabItem>
     <TabItem link={`${props.url}/science`} >Science</TabItem>
