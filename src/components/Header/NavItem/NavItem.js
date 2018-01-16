92fa3d4f726081e755e5cde4c464974700c02b91
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 19:33:42 +0700

    padding bottom

--- src/components/Header/NavItem/NavItem.js
+++ src/components/Header/NavItem/NavItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const navItem = (props) => (
-    <Link to={props.link} className={`link dim gray f5 ${props.header ? props.header : 'f5-ns'} pa2 dib mr3 helvetica`}>
+    <Link to={props.link} className={`link dim gray f5 ${props.header ? props.header : 'f5-ns'} pa2 dib mr3 helvetica pb2`}>
       {props.children}
     </Link>
 );
