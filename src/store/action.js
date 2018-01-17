13e39c375eaec330a7321bc51dc62d667b08c6e8
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-18 00:39:30 +0700

    uncomment & add SUBMIT_QUIZ & CHANGE_FORM

--- src/store/action.js
+++ src/store/action.js
@@ -1,5 +1,5 @@
-// export const INPUT_QUIZ_TITLE = 'INPUT_QUIZ_TITLE';
-// export const INPUT_QUIZ_DATE = 'INPUT_QUIZ_DATE';
-// export const INPUT_QUIZ_SCORE = 'INPUT_QUIZ_SCORE';
-
-export const GET_NILAI = 'GET_NILAI';
+export const INPUT_QUIZ_TITLE = 'INPUT_QUIZ_TITLE';
+export const INPUT_QUIZ_DATE = 'INPUT_QUIZ_DATE';
+export const INPUT_QUIZ_SCORE = 'INPUT_QUIZ_SCORE';
+export const SUBMIT_QUIZ = 'SUBMIT_QUIZ';
+export const CHANGE_FORM = 'CHANGE_FORM';
