eb1acaa87e79f2fc52f1b0275082de873e6da2c7
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-17 23:36:19 +0700

    assign data to chartData from redux

--- src/containers/Stats/Stats.js
+++ src/containers/Stats/Stats.js
@@ -1,62 +1,82 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Bar, Line, Pie, Radar } from 'react-chartjs-2';
 
 class Stats extends Component {
   
   state = {
     chartData: {
-        labels: ['w0', 'w1','w2', 'w3', 'w4'],
-        datasets: [{
-          label: 'Math',
-          data: [0, 80, 65, 100, 85],
-          // backgroundColor: 'green'
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.6)',
-            'rgba(54, 162, 235, 0.6)',
-            'rgba(255, 206, 86, 0.6)',
-            'rgba(75, 192, 192, 0.6)',
-            'rgba(153, 102, 255, 0.6)',
-            'rgba(255, 159, 64, 0.6)',
-            'rgba(255, 99, 132, 0.6)'
-          ],
-          borderWidth: 1,
-          borderColor: '#777',
-          hoverBorderWidth: 3,
-          hoverBorderColor: '#000'
-        }] 
+      labels: ['w0', 'w1','w2', 'w3', 'w4'],
+      datasets: [{
+        data: [0, 80, 65, 100, 85],
+        borderWidth: 3,
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: 'rgb(64,139,201)',
+        hoverBorderWidth: 3,
+        hoverBorderColor: '#000'
+      }] 
+    },
+    options: {
+      maintainAspectRatio: true,
+      title: {
+        display: true,
+        text: 'Weekly Math Quiz Grade',
+        fontSize: 25
       },
-      options: {
-        maintainAspectRatio: true,
-        title: {
-          display: true,
-          text: 'Weekly Math Quiz Grade',
-          fontSize: 25
-        },
-        legend: {
-          display: false,
-          position: 'right',
-          labels: {
-            fontColor: '#000'
-          }
-        },
-        layout: {
-          padding: {
-            left: 50,
-            right: 50,
-            bottom: 0,
-            top: 0
-          }
-        },
-        tooltips: {
-          enabled: true
+      legend: {
+        display: false,
+        position: 'right',
+        labels: {
+          fontColor: '#000'
         }
+      },
+      layout: {
+        padding: {
+          left: 50,
+          right: 50,
+          bottom: 0,
+          top: 0
+        }
+      },
+      tooltips: {
+        enabled: true
       }
     }
+  }
+
+  componentDidMount() {
+    this.updateChartData();
+  }
+
+  getLabelsArray = () => this.props.quizes.map(quiz => quiz.quiz_title);
+
+  getNilaiArray = () => this.props.quizes.map(quiz => parseFloat(quiz.value));
+
+  updateChartData = () => {
+    const labelArr = (this.getLabelsArray());
+    labelArr.splice(0,0,'0');
+
+    const nilaiArr = (this.getNilaiArray());
+    nilaiArr.splice(0,0,0)
+
+    const newChartData = {
+      ...this.state.chartData,
+      labels: labelArr,
+      datasets: [{
+        ...this.state.chartData.datasets[0],
+        data: nilaiArr
+      }]
+    };
+
+    this.setState({
+      chartData: newChartData
+    })
+  } 
 
   render() {
     return (
       <div className="vh-100 w-70 mt5 pt2 center">
-        <Bar 
+        <Line 
           data={this.state.chartData}
           options={this.state.options}
           width={700}
@@ -67,4 +87,10 @@ class Stats extends Component {
   }
 }
 
-export default Stats;
+const mapStateToProps = state  => {
+  return {
+    quizes: state.quizes
+  }
+}
+
+export default connect(mapStateToProps)(Stats);
