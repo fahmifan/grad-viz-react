3acdd399a032274f9a40f90529b1688f7b334947
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-17 23:34:00 +0700

    Make const actionType in action.js

---
+++ src/store/action.js
@@ -0,0 +1,5 @@
+// export const INPUT_QUIZ_TITLE = 'INPUT_QUIZ_TITLE';
+// export const INPUT_QUIZ_DATE = 'INPUT_QUIZ_DATE';
+// export const INPUT_QUIZ_SCORE = 'INPUT_QUIZ_SCORE';
+
+export const GET_NILAI = 'GET_NILAI';
