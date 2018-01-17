785679b5114d37ed56d65c321ce6b9b5eeb86e57
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-17 18:43:45 +0700

    Add line break and conditionally insert .active css class

--- src/components/Tabs/TabItem/TabItem.js
+++ src/components/Tabs/TabItem/TabItem.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const tabItem = (props) => (
-  <Link to={props.link} className="w3 pointer sans-serif b f5 mb0 pv2 ph3 br1 gray link no-underline hover-blue hover-bg-white">{props.children}</Link>
+    <Link 
+      to={props.link} 
+      className={`${props.active ? `active`:null } pointer sans-serif b f5 mb0 pv2 ph3 br1 gray link no-underline hover-blue hover-bg-white`}
+    >{props.children}
+    </Link>
 );
 
 export default tabItem;
