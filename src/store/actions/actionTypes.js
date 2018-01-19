db69934e4dd11d3d86068e041985e95dd276a4ed
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-20 06:05:07 +0700

    made separate actionTypes

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const INIT_STUDENTS =  'INIT_STUDENTS';
+
+export const INPUT_QUIZ_TITLE = 'INPUT_QUIZ_TITLE';
+export const INPUT_QUIZ_DATE = 'INPUT_QUIZ_DATE';
+export const INPUT_QUIZ_SCORE = 'INPUT_QUIZ_SCORE';
+export const SUBMIT_QUIZ = 'SUBMIT_QUIZ';
+export const CHANGE_FORM = 'CHANGE_FORM';
