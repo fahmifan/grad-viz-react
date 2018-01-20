42a7846a73c4329f20c02f0ea80798e60a23741b
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-20 09:41:35 +0700

    add case to handle fetch students from API

--- src/store/reducers/students.js
+++ src/store/reducers/students.js
@@ -1,11 +1,30 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initState = {
-  students: null
+  students: null,
+  error: null,
+  loading: false
 }
 
 const reducer = (state = initState, action) => {
   switch(action.type) {
+    case actionTypes.INIT_STUDENTS: 
+      return {
+        ...state,
+        loading: true
+      }
+    case actionTypes.INIT_STUDENTS_SUCCESS:
+      return {
+        ...state,
+        students: action.students,
+        loading: false
+      }
+    case actionTypes.INIT_STUDENTS_FAIL:
+     return {
+      ...state,
+      loading: false,
+      error: action.error
+     }
     default: return state
   }
 }
