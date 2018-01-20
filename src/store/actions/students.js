24d6f6b8df798df3e678564719d9b0b6df69a4e5
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-20 09:19:57 +0700

    add async fetch using axios-students

--- src/store/actions/students.js
+++ src/store/actions/students.js
@@ -1,14 +1,36 @@
 import * as actionTypes from './actionTypes';
+import axios from '../../axios-students';
 
-export const initStudents = (students) => {
+export const initStudents = () => {
   return {
-    type: actionTypes.INIT_STUDENTS,
+    type: actionTypes.INIT_STUDENTS
+  }
+}
+
+export const initStudentsSuccess = (students) => {
+  return {
+    type: actionTypes.INIT_STUDENTS_SUCCESS,
     students: students
   }
 }
 
-export const fetchStudents = () => {
+export const initStudentsFail = (error) => {
   return {
+    type: actionTypes.INIT_STUDENTS_FAIL,
+    error: error
+  }
+} 
 
+export const fetchStudents = dispatch => {
+  return dispatch => {
+    dispatch(initStudents());
+    axios.get('/student')
+      .then(res => {
+        dispatch(initStudentsSuccess(res.data));
+        console.log(res.data);
+      })
+      .catch(error => {
+        dispatch(initStudentsFail(error));
+      })
   }
 }
